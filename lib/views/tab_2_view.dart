f0c1f9e7a25f1c8ac084e0fbcfb251edec9a774e
bisketashwin <bisketashwin@gmail.com>
2023-09-28 16:48:18 +0530

    Layout widget visual changes
    created a separate widget to show icon with able like ticket, transport, date and time etc.

--- lib/views/tab_2_view.dart
+++ lib/views/tab_2_view.dart
@@ -1,5 +1,8 @@
+// import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
 import 'package:material3_app/theme/custom_color.dart';
 import '../utils/controllers/commodity_ticket_controller.dart';
 import '../utils/models/commodity_ticket_model.dart';
@@ -28,20 +31,22 @@ class _Tab2State extends State<Tab2> {
         padding: const EdgeInsets.only(left: 20.0, right: 20.0),
         child: ListView(
           shrinkWrap:
-              true, //TODO: these 3 are super critical properties for nested lsit views to wrok
+              true, //NOTE: these 3 are super critical properties for nested lsit views to wrok
           children: [
             ListView.builder(
-              shrinkWrap:
-                  true, //TODO:  these 3 are super critical properties for nested lsit views to wrok
-              primary:
-                  false, //TODO:  these 3 are super critical properties for nested lsit views to wrok
+              shrinkWrap: true,
+
+              ///NOTE:  these 3 are super critical properties for nested lsit views to wrok
+              primary: false,
+
+              ///NOTE:  these 3 are super critical properties for nested lsit views to wrok
               itemCount: commodityTickets.length,
               itemBuilder: (context, index) {
                 // return Text(commodityTickets[index].companyName);
                 return commodityTicketCard(context, commodityTickets, index);
               },
             ),
-            CardWithBanner(context),
+            cardWithBanner(context),
           ],
         ),
       ),
@@ -50,98 +55,196 @@ class _Tab2State extends State<Tab2> {
 }
 
 Widget commodityTicketCard(context, commodityTickets, index) {
+  bool showDetails = false;
   var commodityTicket = commodityTickets[index];
   var pickAd = commodityTicket.pickUpAddress;
   var destAd = commodityTicket.destinationAddress;
   var color1 = Theme.of(context).colorScheme.onBackground.withOpacity(0.6);
   var defaultTextStyle = Theme.of(context).textTheme.bodySmall;
-  return Card(
-    child: Column(
-      children: [
-        topBannerStatus(context, 1),
-        Padding(
-          padding:
-              const EdgeInsets.only(left: 16, right: 16, bottom: 16, top: 5),
-          child: Column(
-            children: [
-              DefaultTextStyle(
-                style: defaultTextStyle!,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Ticket Number',
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          WidgetSpan(
-                            child: FaIcon(
-                              FontAwesomeIcons.ticket,
-                              size: 20,
-                              color: color1,
-                            ),
-                          ),
-                          TextSpan(
-                            text: ' ${commodityTicket.ticketNumber}',
-                            style:
-                                Theme.of(context).textTheme.bodyLarge!.copyWith(
-                                      fontWeight: FontWeight.w600,
-                                      color: color1,
-                                    ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Text(
-                      '${commodityTicket.commodity}  ${commodityTicket.quantity}',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    Text(commodityTicket.commodityOwner.firmName,
-                        style: Theme.of(context).textTheme.bodyLarge),
-                    Text(
-                        'Job Created by \n${commodityTicket.contactPerson} ( ${commodityTicket.phoneNumber} )'),
+  return Center(
+    child: GestureDetector(
+      onDoubleTap: () {
+        showDetails = !showDetails;
+        debugPrint('showDetails = $showDetails');
+      },
+      child: Card(
+        child: Column(
+          children: [
+            topBannerStatus(context, 1),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 16, right: 16, bottom: 16, top: 5),
+              child: Column(
+                children: [
+                  DefaultTextStyle(
+                    style: defaultTextStyle!,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        iconLabel(
+                          label: 'Ticket Number',
+                          context: context,
+                          mainText: commodityTicket.ticketNumber,
+                          textSize: 'bodyMedium',
+                          color: color1,
+                        ),
+                        Text(
+                          '${commodityTicket.commodity}  ${commodityTicket.quantity}',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        Text(commodityTicket.commodityOwner.firmName,
+                            style: Theme.of(context).textTheme.bodyLarge),
+                        Visibility(
+                          visible: showDetails,
+                          child: Text(
+                              'Job Created by \n${commodityTicket.contactPerson} ( ${commodityTicket.phoneNumber} )'),
+                        ),
+                        const Divider(),
+                        Text(
+                          'Pick up point - ${pickAd.villageOrTaluk}',
+                          style: Theme.of(context).textTheme.titleSmall,
+                        ),
+                        Text(
+                          DateFormat('h:mm a  --  d MMM yyyy')
+                              .format(commodityTicket.pickupDate),
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleSmall!
+                              .copyWith(color: color1),
+                        ),
+                        iconLabel(
+                          label: 'Transport Provided by',
+                          type: 'Transport',
+                          context: context,
+                          mainText: commodityTicket.transportType,
+                          textSize: 'bodyMedium',
+                          color: color1,
+                        ),
+                        Text(pickAd.firmName),
+                        Text(pickAd.street),
+                        // Text(pickAd.villageOrTaluk),
+                        Text(
+                            '${pickAd.zilla}, ${pickAd.state} - ${pickAd.pincode}'),
+                        //Text(pickAd.state),
 
-                    Divider(),
-                    Text(
-                      'Pick up point - ${pickAd.villageOrTaluk}',
-                      style: Theme.of(context).textTheme.titleSmall,
+                        const Divider(),
+                        Text(
+                          'Destination - ${destAd.villageOrTaluk}',
+                          style: Theme.of(context).textTheme.titleSmall,
+                        ),
+                        Text(destAd.firmName),
+                        Text(destAd.street),
+                        // Text(destAd.villageOrTaluk),
+                        Text(
+                            '${destAd.zilla}, ${destAd.state} -${destAd.pincode}'),
+                      ],
                     ),
-                    Text(pickAd.firmName),
-                    Text(pickAd.street),
-                    // Text(pickAd.villageOrTaluk),
-                    Text(
-                        '${pickAd.zilla}, ${pickAd.state} - ${pickAd.pincode}'),
-                    //Text(pickAd.state),
-
-                    Text(
-                      '${DateFormat('h:mm a  --  d MMM yyyy').format(commodityTicket.pickupDate)}',
-                      style: Theme.of(context).textTheme.titleSmall,
-                    ),
-                    Divider(),
-                    Text(
-                      'Destination - ${destAd.villageOrTaluk}',
-                      style: Theme.of(context).textTheme.titleSmall,
-                    ),
-                    Text(destAd.firmName),
-                    Text(destAd.street),
-                    // Text(destAd.villageOrTaluk),
-                    Text('${destAd.zilla}, ${destAd.state} -${destAd.pincode}'),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
+      ),
     ),
   );
 }
 
-Widget CardWithBanner(BuildContext context) {
-  var color1 = Theme.of(context).colorScheme.onBackground.withOpacity(0.6);
-  return Container(
+Widget iconLabel(
+    {String type = '',
+    String label = '',
+    required context,
+    String mainText = '',
+    required String textSize,
+    required Color color}) {
+  TextStyle style;
+  double iconSize;
+  IconData myIcon;
+  switch (type) {
+    case 'Date Time':
+      myIcon = FontAwesomeIcons.calendarDay;
+      style = Theme.of(context).textTheme.bodyLarge!.copyWith(
+            fontWeight: FontWeight.w600,
+            color: color,
+          );
+      iconSize = 20.0;
+      break;
+    case 'Transport':
+      myIcon = FontAwesomeIcons.truck;
+      style = Theme.of(context).textTheme.bodyLarge!.copyWith(
+            fontWeight: FontWeight.w600,
+            color: color,
+          );
+      iconSize = 20.0;
+      break;
+    default:
+      // by default it 'Ticket Number'
+      myIcon = FontAwesomeIcons.ticket;
+      style = Theme.of(context).textTheme.bodyLarge!.copyWith(
+            fontWeight: FontWeight.w600,
+            color: color,
+          );
+      iconSize = 20.0;
+  }
+  switch (textSize) {
+    case 'bodyMedium':
+      style = Theme.of(context).textTheme.bodyMedium!.copyWith(
+            fontWeight: FontWeight.w600,
+            color: color,
+          );
+      iconSize = 16.0;
+      break;
+    case 'Transport':
+      style = Theme.of(context).textTheme.bodyLarge!.copyWith(
+            fontWeight: FontWeight.w600,
+            color: color,
+          );
+      iconSize = 20.0;
+      break;
+    default:
+      // by default it 'bodyLarge'
+      style = Theme.of(context).textTheme.bodyLarge!.copyWith(
+            fontWeight: FontWeight.w600,
+            color: color,
+          );
+      iconSize = 20.0;
+  }
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Visibility(
+        visible: label == '' ? false : true,
+        child: Text(label),
+      ),
+      RichText(
+        text: TextSpan(
+          children: [
+            WidgetSpan(
+              child: FaIcon(
+                myIcon,
+                size: iconSize,
+                color: color,
+              ),
+            ),
+            TextSpan(
+              text: ' $mainText',
+              style: style,
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget cardWithBanner(BuildContext context) {
+  // var color1 = Theme.of(context).colorScheme.onBackground.withOpacity(0.6);
+  bool showDetails = false;
+  return GestureDetector(
+    onTap: () {
+      showDetails = !showDetails;
+      debugPrint('showDetails = $showDetails');
+    },
     child: Card(
       color: Colors.orange,
       elevation: 10,
@@ -153,9 +256,19 @@ Widget CardWithBanner(BuildContext context) {
         // mainAxisSize: MainAxisSize.min,
         children: [
           topBannerStatus(context, 1),
-          SizedBox(
-            height: 100,
-          )
+          Padding(
+            padding: const EdgeInsets.only(top: 20, bottom: 20),
+            child: Text(
+                'This would be visible all time. showDetails = $showDetails'),
+          ),
+          Visibility(
+            visible: showDetails,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              child:
+                  Text('This would show only if showDetails is $showDetails'),
+            ),
+          ),
         ],
       ),
     ),
@@ -165,10 +278,10 @@ Widget CardWithBanner(BuildContext context) {
 Widget topBannerStatus(BuildContext context, int status) {
   var color1 = Theme.of(context).colorScheme.onBackground.withOpacity(0.6);
   // var color2 = Theme.of(context).extension<CustomColors>()!.sourceCustomcolor3;
-  Color colorNotStarted = Color(0xFF787878);
-  Color colorInProgress = Color(0xFFFFC268);
-  Color colorNeedAttention = Color(0xFFE24646);
-  Color colorCompleted = Color(0xFF46E27D);
+  Color colorNotStarted = const Color(0xFF787878);
+  Color colorInProgress = const Color(0xFFFFC268);
+  Color colorNeedAttention = const Color(0xFFE24646);
+  Color colorCompleted = const Color(0xFF46E27D);
 
   Color statusColor;
   IconData statusIcon;
