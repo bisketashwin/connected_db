3d775d9bda42e3cf1e2e9e973989c7853cd36fcd
bisketashwin <bisketashwin@gmail.com>
2023-09-30 17:28:05 +0530

    Hide bottom navigation
    the bottom navigation hides on scroll up and come back on scroll down this is different from home_view
    NOTE: tab one is not implemented with this approach yet

--- lib/app_views.dart
+++ lib/app_views.dart
@@ -1,13 +1,16 @@
 // import 'utils/controllers/commodity_ticket_controller.dart';
 // ignore_for_file: constant_identifier_names
 
+// import 'package:material3_app/views/home_2_view.dart';
+import 'package:material3_app/views/home_2_view.dart';
+import 'package:material3_app/views/home_3_view.dart';
 import 'package:material3_app/views/log_in_view.dart';
 
 import 'app_routes.dart';
 import 'package:get/get.dart';
 
 // import 'utils/controllers/user_controller.dart';
-import 'views/home_view.dart'; // Import for GetX
+// import 'views/home_view.dart'; // Import for GetX
 
 // IMP: Get X to work: this file is important
 
@@ -24,7 +27,7 @@ class AppScreens {
     // GetPage(name: Routes.SIGNUP, page: () => SignUpView()),
     GetPage(
       name: Routes.HOME,
-      page: () => const HomeView(),
+      page: () => HomeView2(),
     ),
     // GetPage(name: Routes.SETTINGS, page: () => const Settings()),
     // GetPage(name: Routes.PROFILE_EDIT, page: () => const ProfileEdit()),
